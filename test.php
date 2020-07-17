bfa1332408078ecbf4f889cf8dcae73b52c8b27a
18412 <18412@toba.kosen-ac.jp>
2020-07-17 22:00:18 +0900

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 // defineの値は環境によって変えてください。
-define('HOSTNAME', 'localhost');
+define('HOSTNAME', '127.0.0.1');
 define('DATABASE', 'library_db');
 define('USERNAME', 'azure');
 define('PASSWORD', ' ');
