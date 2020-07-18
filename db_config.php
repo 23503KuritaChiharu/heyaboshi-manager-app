782384357fd745fbab23f140d4bb89deea687285
18412 <18412@toba.kosen-ac.jp>
2020-07-18 22:35:21 +0900

    test

--- db_config.php
+++ db_config.php
@@ -1,22 +1,14 @@
 <?php
-  $connectstr_dbhost = '';
-  $connectstr_dbname = '';
-  $connectstr_dbusername = '';
-  $connectstr_dbpassword = '';
+$servername = "localhost";
+$username = "root";
+$password = " ";
 
-  foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-  }
-
-  define('DB_DATABASE', 'library_db');
-  define('DB_USERNAME', $connectstr_dbusername);
-  define('DB_PASSWORD', $connectstr_dbpassword);
-  define('PDO_DSN', 'mysql:host='.$connectstr_dbhost.';dbname='. DB_DATABASE . ';charset=utf8;');
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
