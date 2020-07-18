d57b52a9f3bce4b536ce0b30fca142f255460915
18412 <18412@toba.kosen-ac.jp>
2020-07-18 18:21:32 +0900

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 // defineの値は環境によって変えてください。
-define('HOSTNAME', 'RD0004FFD12EB6');
+define('HOSTNAME', '127.0.0.1');
 define('DATABASE', 'sentakumono');
 define('USERNAME', 'root');
 define('PASSWORD', ' ');
