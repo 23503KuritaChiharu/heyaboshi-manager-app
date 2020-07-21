b5adcce18c04ff2f56a9d7290181a48b87f905c0
18412 <18412@toba.kosen-ac.jp>
2020-07-21 18:08:02 +0900

    温度のグラフを表示

--- graph_page.php
+++ graph_page.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/graph_page.css?<?php echo date('Ymd-His'); ?>">
+    <link rel="stylesheet" href="css/graph_page.css?<?php echo date('Ymd-His'); ?>">
     
     <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p&display=swap" rel="stylesheet">
   </head>
@@ -22,15 +22,26 @@
     <label class="tab_item" for="programming">湿度</label>
     <input id="design" type="radio" name="tab_item">
     <label class="tab_item" for="design">含水量</label>
+
     <div class="tab_content" id="all_content">
-        温度のグラフがここに入ります
+      <?php
+        // include 'ondo.php';
+        // include(dirname(__FILE__).'/graph/ondo.php');
+        include __DIR__. '/graph/ondo.php';
+      ?>
     </div>
     <div class="tab_content" id="programming_content">
-        湿度のグラフがここに入ります
-</div>
+      <?php
+        // include '/graph/situdo.php';
+        // include __DIR__. '/graph/situdo.php';
+      ?>
+    </div>
     <div class="tab_content" id="design_content">
-        含水量の内容がここに入ります
-</div>
+      <?php
+        // include __DIR__. '/graph/water.php';
+      ?>
+    </div>
+  </div>
   </body>
 
   </html>
