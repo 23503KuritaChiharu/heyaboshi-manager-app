9e04aee7c9ed667f4701f7b5bea26a25cee63667
18412 <18412@toba.kosen-ac.jp>
2020-08-15 14:23:37 +0900

    test

--- test2.php
+++ test2.php
@@ -1,25 +1,37 @@
 <?php
-  include 'db_config.php';
-
-  $products = array();
-
-  try
-  {
-     // connect
-     $db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD);
-     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-     // 商品一覧を取得
-     $stmt = $db->query("SELECT * FROM result");
-     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-     $db = null;
-  }
-  catch(PDOException $e)
-  {
-   echo $e->getMessage();
-   exit;
-  }
-
+// Cosmos DBに接続するライブラリのパスを指定する
+require_once '/path/to/phpdocumentdb.php';
+ 
+// DocmentDBのホスト名を指定する
+$host = 'https://sentakumono.documents.azure.com:443/';
+ 
+// DocumentDBのマスターキーを指定する
+$master_key = 'Obix2WxJ7aahCsHAKklkVrtdvsJVKZqgaCJ5iB89izHJ2IVw9iI7EiaTa99Gtn3fSJ8L0MWeipTcS3sAv7uiyg==';
+ 
+// DocumentDBに接続する
+$documentdb = new DocumentDB($host, $master_key,$debug = true);
+ 
+// データベースを選択する
+$db = $documentdb->selectDB("testdb");
+ 
+// コレクションを選択する
+$col = $db->selectCollection("testcol");
+ 
+// クエリを実行する
+$result = $col->query("SELECT * FROM c");
+ 
+// JSONをデコードする
+$result_json = json_decode($result);
+ 
 ?>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>CosmosDB</title>
+  </head>
+ 
+  <body>
+    ID:<?php echo $result_json->Documents[0]->id; ?><br>
+    Name:<?php echo $result_json->Documents[0]->name; ?>
+  </body>
+</html>
