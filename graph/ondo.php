2e6e3307d4666e742eae2944389a9b8d24d23a22
18412 <18412@toba.kosen-ac.jp>
2020-08-23 22:34:38 +0900

    test

--- graph/ondo.php
+++ graph/ondo.php
@@ -10,6 +10,29 @@
     <!-- CSS -->
     <link rel="stylesheet" href="css/graph_page.css?<?php echo date('Ymd-His'); ?>">
     
+     <!-- The core Firebase JS SDK is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js"></script>
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+    https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="https://www.gstatic.com/firebasejs/7.18.0/firebase-analytics.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.18.0/firebase-database.js"></script>
+    <script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyDwizDxSbdLAAFrOmm5v0gwyiVVBt8QKYk",
+    authDomain: "heyaboshi-manager.firebaseapp.com",
+    databaseURL: "https://heyaboshi-manager.firebaseio.com",
+    projectId: "heyaboshi-manager",
+    storageBucket: "heyaboshi-manager.appspot.com",
+    messagingSenderId: "767584081695",
+    appId: "1:767584081695:web:ff141bebf62fed516d822a",
+    measurementId: "G-W1K945Y27G"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    </script>
+
   </head>
 
 <body>
@@ -33,18 +56,22 @@
                     pointShape: 'square',
                     chartArea: {width:'50%',height:'80%'}
                 };
-                var chart = new google.visualization.LineChart(target);
-                data = new google.visualization.arrayToDataTable([
-                    ['時間', '温度'],
-                    ['0', 0],
-                    ['1', 35],
-                    ['2', 28],
-                    ['3', 27],
-                    ['4', 23],
-                    ['5', 25],
-                ]);
+                return firebase.database().ref('/devices').chdir('test_device_1').once('value').then(function(snapshot) {
+                    console.log(snapshot.val());
+                    var chart = new google.visualization.LineChart(target);
+                    data = new google.visualization.arrayToDataTable([
+                        ['時間', '温度'],
+                        ['0', 0],
+                        ['1', 35],
+                        ['2', 28],
+                        ['3', 27],
+                        ['4', 23],
+                        ['5', 25],
+                    ]);
 
-                chart.draw(data, options);
+                    chart.draw(data, options);
+                });
+
             }
 
             google.charts.load('current',{packages: ['corechart']});
