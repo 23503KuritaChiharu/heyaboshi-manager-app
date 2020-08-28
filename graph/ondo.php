84be336bd82d5645f95589fd76dbf8e0ae7a357c
18412 <18412@toba.kosen-ac.jp>
2020-08-28 20:40:49 +0900

    test

--- graph/ondo.php
+++ graph/ondo.php
@@ -84,7 +84,7 @@
         firebase.database().ref('/devices/test_device_1').once('value').then(function(snapshot) {
             datas = snapshot.val();
             console.log(datas);
-            google.charts.setOnLoadCallback(drawChart(datas));
+            // google.charts.setOnLoadCallback(drawChart(datas));
         });
     </script>
 
